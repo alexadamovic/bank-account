9326d1130379f5ce1a9846426062437a230ceed0
Alex Adamovic <alexanderadamovic@gmail.com>
2022-01-25 19:57:18 -0800

    add code to pass third test

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,12 @@ function BankAccount(balance) {
 BankAccount.prototype.deposit = function(moneyIn) {
   this.balance += moneyIn
 };
+
+BankAccount.prototype.withdrawal = function(moneyOut) {
+  this.balance -= moneyOut
+};
+
+$(document).ready(function() {
+  $("form#bank").submit(function(event) {
+    event.preventDefault();
+    let 
